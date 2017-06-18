4060445e06eff7d13113c63f941ac079e712af1f
Dylan Piercey <pierceydylan@gmail.com>
2017-06-18 15:34:38 -0700

    Add scrollIntoView options (#2)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = scrollToAnchor
 
-function scrollToAnchor (anchor) {
+function scrollToAnchor (anchor, options) {
   if (anchor) {
     try {
       var el = document.querySelector(anchor)
-      if (el) el.scrollIntoView(true)
+      if (el) el.scrollIntoView(options)
     } catch (e) {}
   }
 }
